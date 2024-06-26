8fd8d4dc8e9d37fe896994331e5a8748d0e79426
Alleaxx <Allexx1112@gmail.com>
2024-06-26 11:55:34 +0300

    Обновление симулятора до версии 7.3

--- _content/RumineLibrary.RazorUI/exampleJsInterop.js
+++ _content/RumineLibrary.RazorUI/exampleJsInterop.js
@@ -13,3 +13,58 @@ export function scrollTo(elementId, offset) {
 export function writeTextClipboard(text) {
     navigator.clipboard.writeText(text);
 }
+
+export function installNewsNavigationHighlight() {
+    const highlightClass = "nav-highlight";
+    const logoHeight = document.querySelector(".main-header").clientHeight;
+    const headerNewsHeight = 75;
+    const freeSpace = 40;
+
+    var fixedHeight = headerNewsHeight + logoHeight + freeSpace;
+    fixedHeight = -fixedHeight;
+
+    window.addEventListener('scroll', function () {
+        var currentElement;
+        var navigatableElements = document.querySelectorAll(".navigatable");
+        if (navigatableElements.length == 0) {
+            return;
+        }
+
+        //Находим первый пролистанный
+        var minDiffElement = {
+            element: undefined,
+            difference: 100000
+        };
+        navigatableElements.forEach(element => {
+            var viewportOffset = element.getBoundingClientRect();
+            var offsetY = viewportOffset.top + fixedHeight;
+
+            //Больше 0 - еще впереди.
+            //Равно 0 - у верхнего края экрана
+            //Меньше 0 - уже начата прокрутка
+            const offsetDifference = Math.abs(offsetY);
+            if (offsetDifference < minDiffElement.difference && offsetY < 0) {
+                minDiffElement.element = element;
+                minDiffElement.difference = offsetDifference;
+            }
+        });
+
+        const newCurrentElement = minDiffElement.element;
+        if (newCurrentElement && newCurrentElement != currentElement) {
+            document.querySelectorAll("." + highlightClass).forEach(e => e.classList.remove(highlightClass));
+
+            currentElement = newCurrentElement;
+            const id = newCurrentElement.id;
+            const navID = "nav-" + id;
+            const newNavElement = document.querySelector("#" + navID);
+            if (newNavElement) {
+                newNavElement.classList.add(highlightClass)
+            }
+            else {
+                console.log(newCurrentElement);
+                console.log("nav not found " + navID)
+                console.log(navID);
+            }
+        }
+    });
+}
