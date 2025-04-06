9ff5d19fd97f0f281834d3fdfb3418ea34d3d6e4
Alleaxx <Allexx1112@gmail.com>
2025-04-06 11:16:59 +0300

    Обновление до 8.1

--- _content/RumineLibrary.RazorUI/exampleJsInterop.js
+++ _content/RumineLibrary.RazorUI/exampleJsInterop.js
@@ -1,9 +1,3 @@
-// This is a JavaScript module that is loaded on demand. It can export any number of
-// functions, and may import other JavaScript modules if required.
-
-export function showPrompt(message) {
-  return prompt(message, 'Type anything here');
-}
 export function scrollTo(elementId, offset) {
     const element = document.getElementById(elementId);
     window.scrollTo(0, element.offsetTop - offset);
